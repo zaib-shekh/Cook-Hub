d2419c8391c1598e28b9139e0abbde27b952e46c
zaib-shekh <zaib.shekh313@gmail.com>
2025-03-19 13:15:02 +0530

    little change

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown"
 export default function ClaudeRecipe(props) {
     return (
         <section className="suggested-recipe-container" aria-live="polite">
-            <h2>Chef Claude Recommends:</h2>
+            <h2>Cook Hub Recommends:</h2>
             <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
